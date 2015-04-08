9ea4f03e9014ecf98b519e2c7f64509d97f465de
smashfligaeta <dgrayson11@gmail.com>
2015-04-08 11:28:37 -0700

    Matt Daemon now logs to syslog

--- daemon.c
+++ daemon.c
@@ -1,13 +1,14 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <syslog.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <string.h>
 
 int main(int argc, char* argv[]) {
-	FILE *fp= NULL;
+	//FILE *fp= NULL;
 	pid_t process_id = 0;
 	pid_t sid = 0;
 	// Create child process
@@ -33,20 +34,22 @@ int main(int argc, char* argv[]) {
 		exit(1);
 	}
 	// Change the current working directory to root.
-	chdir("/");
+	chdir("$HOME");
 	// Close stdin. stdout and stderr
 	close(STDIN_FILENO);
 	close(STDOUT_FILENO);
 	close(STDERR_FILENO);
 	// Open a log file in write mode.
-	fp = fopen ("Log.txt", "w+");
+	//fp = fopen ("Log.txt", "w+");
 	while (1){
 		//Dont block context switches, let the process sleep for some time
 		sleep(1);
-		fprintf(fp, "Logging info...\n");
-		fflush(fp);
+		syslog(LOG_INFO, "Matt Daemon, reporting for duty");
+		//fprintf(fp, "Logging info...\n");
+		//fflush(fp);
 		// Implement and call some function that does core work for this daemon.
 	}
-	fclose(fp);
+	//closelog();
+	//fclose(fp);
 	return (0);
 }
